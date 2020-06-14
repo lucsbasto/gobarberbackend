9161b170ca0b309f43c512a2cd14e21b576a7dad
Lucas <lucsbasto@gmail.com>
2020-06-14 16:54:04 -0300

    adicionando eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
